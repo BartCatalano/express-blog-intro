efdb7c6770e2ecc02add7b88092312b4ed5db32a
BartCatalano <catalanobartolomeo@gmail.com>
2024-12-02 14:45:10 +0100

    feat: array fatto, rotte create. prova funzionalità (WIP)

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
+// Imposto framework express 
+const express = require(`express`);
+const app = express();
+const port = 3001;
+// creo rotta base 
+app.get("/", (req, res) => {
+    res.send("Server del mio blog");
+});
+//  creo rotta che restituisce lista dei post e conteggio
+app.get(`/bacheca`,(req, res) =>{
+    const data = {
+        elenco: bacheca,
+        totale: bacheca.length
+    }
+    res.jason(data);
+});
 
+
+// creo rotta di apertura server
+app.listen(port,() =>{
+
+});
